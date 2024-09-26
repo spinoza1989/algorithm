1d7fd1c18ac15c99a00473b659da8617b7821b5d
Leo Wang <wangjil@delawareconsulting.com>
2024-09-26 09:39:42 +0800

    firstAdd

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void Print32Str(int n)
+{
+    for (int i = 31; i >=0; i--)
+    {
+        var str = ( n & (1 << i)) == 0 ? "0" : "1";
+        Console.Write(str);
+    }
+}
+
+
+Print32Str(2);
