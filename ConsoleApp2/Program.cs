287b86a73a699d355006d4b592c15a85c51ebab7
Leo Wang <wangjil@delawareconsulting.com>
2024-09-29 17:24:37 +0800

    临时测试

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var str = "飞秒激光角膜屈光治疗机 | VisuMax,准分子激光系统 | MEL90,准分子激光系统 | MEL80,准分子激光系统 | MEL 80,视野分析仪 | 830,角膜地形图系统 | 9000,光相干断层扫描仪 | 5000,光相干断层扫描仪 | 500,一次性使用无菌眼内探针 | 20 gauge angled,眼科光学生物测量仪 | IOLMaster 700";
+
+Console.WriteLine(str.Length);
